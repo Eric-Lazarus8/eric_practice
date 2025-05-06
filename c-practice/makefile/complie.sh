f967372d5f93f6e62c3a895480e5cf588c3b1e37
eric.d <eric@DZO>
2025-05-06 16:22:21 +0530

    c-practice/

---
+++ c-practice/makefile/complie.sh
@@ -0,0 +1,11 @@
+ls
+pwd
+gcc hello.c
+./a.out
+echo  "DZO" 
+    file="/home/eric/ppractice/eric_practice/c-practice/makefile"
+    if [ -f "$1" ]; then
+      echo "File exists"
+    else
+      echo "File does not exist"
+    fi
