0c4141281b07722d9a6304757f1ab2438bf80b1b
eric.d <eric@DZO.localdomain>
2025-05-30 15:23:13 +0530

    c-practice

---
+++ c-practice/practice/cpp.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+static inline int add(int a, int b)
+{
+	return a+b;
+}
+int main()
+{
+	int result = add(1,2);
+}
---
+++ c-practice/practice/preprocessor.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#define PI 3.1415
+
+int main() {
+    printf("Value of PI: %f\n", PI);
+    return 0;
+}
