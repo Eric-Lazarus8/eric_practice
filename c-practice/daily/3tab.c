d22ad5818667fd45ea9b53f58c3eb6577388238f
Eric-Lazarus8 <ericlazarus888@gmail.com>
2025-08-14 15:59:15 +0530

    c

---
+++ c-practice/daily/3tab.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+        for(int i =1;i<=10;i++)
+        {
+                printf("3x%d=%d\n",i,(i<<1)+i);
+        }
+}
---
+++ c-practice/daily/fun.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#define squ(x) (x)*(x)
+int main()
+{
+	printf("%d\n",squ(2));
+}
